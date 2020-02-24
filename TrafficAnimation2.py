643b1ee3aba4c784d545c78a1035b6f238bc150e
MorphlingEd <55635778+MorphlingEd@users.noreply.github.com>
2020-02-24 21:17:00 +0000

    Update TrafficAnimation2.py

--- TrafficAnimation2.py
+++ TrafficAnimation2.py
@@ -46,10 +46,13 @@ class TrafficAnimation2(object):
         ax = plt.axes()
         self.patches = []
 
+        height = TrafficAnimation2.HEIGHT
+        radius = TrafficAnimation2.RADIUS
+
         """Initializing circles in cells where there are cars"""
         for i in range(0,self.xmax):
             if self.r.cells[i] == 1:
-                newCircle = plt.Circle((i,0.5 * TrafficAnimation2.HEIGHT + TrafficAnimation2.RADIUS),TrafficAnimation2.RADIUS,color = 'r',animated = True)
+                newCircle = plt.Circle((i,0.5 * height + radius),radius,color = 'r',animated = True)
                 self.patches.append(newCircle)
                 ax.add_patch(newCircle)
         
@@ -60,19 +63,13 @@ class TrafficAnimation2(object):
         """ set up the axes"""
         ax.axis('scaled')
         ax.set_xlim(self.xpos, self.xmax)
-        ax.set_ylim(0, TrafficAnimation2.HEIGHT)
+        ax.set_ylim(0, height)
         ax.set_xlabel('cells of the road (length = ' + str(len(self.r.cells)) + ', total number of cars = '+ str(self.r.cars)+')' )
-        ax.set_ylabel('cars in cells ('+str(0.5*TrafficAnimation2.HEIGHT)+' times of 1 or 0)')
-        self.timeText = ax.text(1,TrafficAnimation2.HEIGHT - len(self.r.cells)/20,'')
+        ax.set_ylabel('cars in cells ('+str(0.5*height)+' times of 1 or 0)')
+        self.timeText = ax.text(1,height - min(len(self.r.cells)/10,height/2-2),'',fontsize=7.5)
 
         """create the animator"""
         anim = FuncAnimation(fig, self.animate, init_func = self.init, frames = self.iter, repeat = True, interval = 200, blit = True)
 
         """ show the plot """
         plt.show()
-
-def main():
-    s = TrafficAnimation2(300,1)
-    s.run()
-    
-main()

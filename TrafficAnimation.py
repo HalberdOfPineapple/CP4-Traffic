9088159b8afcd3274b2bb4837074a59ad08cbc0a
MorphlingEd <55635778+MorphlingEd@users.noreply.github.com>
2020-02-24 20:09:40 +0000

    Update TrafficAnimation.py

--- TrafficAnimation.py
+++ TrafficAnimation.py
@@ -5,27 +5,31 @@ from Traffic import road
 
 
 class TrafficAnimation(object):
-    HEIGHT = 50
+    HEIGHT = 50 #Setting the height of graph
 
     def __init__(self,length,density):
+        """Initialize the road's length and density, setting 
+        final x coordinate to be the length of the road"""
         # set initial and final x coordinates of the graph
         self.xpos = 0.0
         self.xmax = length
         
-        # set number of frames
-        self.iter = 500
+        self.iter = 500  #Setting the number of frames
 
-        # initialize a road object 
-        self.r = road(length,density)
+        self.r = road(length,density)        # initialize a road object 
+
     def init(self):
-        # initialiser for animator
+        """initialiser for animator"""
+        self.timeText.set_text('')
         return self.patches
 
     def animate(self, i):
+        """method for iterating to be used in the animator"""
         self.r.move()
-        for j in range(0,100):
+        self.timeText.set_text('The average speed is '+str(self.r.avg_speed[-1])+'\nIteration times: ' + str(i))
+        for j in range(0,len(self.r.cells)):
             self.patches[j].set_height(0.5 * TrafficAnimation.HEIGHT * self.r.cells[j])
-        return self.patches
+        return tuple(self.patches) + tuple([self.timeText])
 
     def run(self):
 
@@ -33,17 +37,19 @@ class TrafficAnimation(object):
         ax = plt.axes()
         self.patches = []
 
-
-        for i in range(0,100):
-            self.patches.append(plt.Rectangle((i,0),1,0.5 * TrafficAnimation.HEIGHT * self.r.cells[i],color = 'r',animated = True))
+        """Generating rectangles in each cell where there is a car to represent the state"""
+        for i in range(0,len(self.r.cells)):
+            self.patches.append(plt.Rectangle((i,0),1,0.5 * TrafficAnimation.HEIGHT * self.r.cells[i],color = 'black',animated = True))
             ax.add_patch(self.patches[i])
 
-        # set up the axes
+        """set up the axes"""
         ax.axis('scaled')
         ax.set_xlim(self.xpos, self.xmax)
         ax.set_ylim(0, TrafficAnimation.HEIGHT)
         ax.set_xlabel('cells of the road (length = ' + str(len(self.r.cells)) + ', total number of cars = '+ str(self.r.cars)+')' )
         ax.set_ylabel('cars in cells ('+str(0.5*TrafficAnimation.HEIGHT)+' times of 1 or 0)')
+        # self.timeText = ax.text(0.02, 0.95, '', transform=ax.transAxes)   
+        self.timeText = ax.text(1,TrafficAnimation.HEIGHT - len(self.r.cells)/20,'')
 
         # create the animator
         anim = FuncAnimation(fig, self.animate, init_func = self.init, frames = self.iter, repeat = True, interval = 200, blit = True)
@@ -52,7 +58,7 @@ class TrafficAnimation(object):
         plt.show()
 
 def main():
-    s = TrafficAnimation(100,0.35)
+    s = TrafficAnimation(100,1)
     s.run()
     
 main()

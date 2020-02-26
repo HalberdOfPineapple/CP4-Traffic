2788d4d777e9b4acbcb42d10f0934a6445e305b3
MorphlingEd <55635778+MorphlingEd@users.noreply.github.com>
2020-02-26 10:10:49 +0000

    Update Traffic.py

--- Traffic.py
+++ Traffic.py
@@ -25,16 +25,18 @@ class road(object):
 
     
     def move(self):
+        """method for a road to update states of each cell according to the given algorithm
+        and return number of moves at this timestep"""
         if self.cars == 0 :
             return 0
-        next = self.cells.copy()
+        next = self.cells.copy()            # make a copy as a template of next timestep's cells
         move = 0
         length = len(self.cells)
         for i in range(0,length):
             if (self.cells[i] == 1):
                 if (self.cells[(i+1)%length] == 0):
                     next[i] = 0
-                    move += 1
+                    move += 1                  # moves increment
                 elif (self.cells[(i+1)%length] == 1):
                     next[i] = 1
             elif (self.cells[i] == 0):
@@ -44,8 +46,9 @@ class road(object):
                 elif (self.cells[(i-1)%length] == 0):
                     next[i] = 0
         self.cells = next
-        self.avg_speed.append(move/self.cars)
-        # print(str(move))
+
+        """Adding this time's average speed into the array of average speeds"""
+        self.avg_speed.append(move/self.cars) 
         return move
 
     
@@ -58,13 +61,14 @@ class road(object):
         """method to present the distribution of cars in the road"""
         print(' '.join(map(str,self.cells)))
 
-    def timePrint(self,times):
-        """Method combining iterate and print road, moving in a certain times with each time 
-        printing the road"""
+    def timePrint(self,times = 10):
+        """Method combining iterate and printRoad, moving in a certain times with each time 
+        printing the road and average speed"""
         self.printRoad()
         for i in range(0,times):
             self.move()
             self.printRoad()
+            print("Average speed of this time step is : " + str(self.avg_speed[-1]))
 
     def steadyAvgSpeed(self):
         """Method used to get the road's average moving speed in steady state"""
@@ -73,7 +77,7 @@ class road(object):
         else:
             control = False
             while not control:
-                self.iterate(max((int)(len(self.cells)/2),10))                                   # Making sure the cars have moved at least twice
+                self.iterate(max((int)(len(self.cells)/2),50))                                   # Making sure the cars have moved at least twice
                 control = (self.avg_speed[-1] == self.avg_speed[-2] and
                         self.avg_speed[-2] == self.avg_speed[-3] and
                         self.avg_speed[-3] == self.avg_speed[-4] and
@@ -82,9 +86,18 @@ class road(object):
     
     @classmethod
     def calSteadyAvgSpeed(cls,length):
+        """class method used to generate an array of steady average speed for roads with certain length
+        but with different density ranging from 0 to 1"""
         densities = np.linspace(0,1,200)
         steadyAvg = []
         for x in densities:
             temp = road(length,x)
             steadyAvg.append(temp.steadyAvgSpeed())
         return steadyAvg
+
+def main():
+    r = road(100,0.35)
+    r.timePrint()
+
+if __name__ == '__main__':
+    main()
